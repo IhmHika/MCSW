7de3f902d2be1a1353e1e788ba79701c632e7d45
IhmHika <aoihika098@gmail.com>
2026-03-01 23:54:02 +0900

    Delete mcws directory

--- mcws/setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Minecraft Bot Setup
-echo [1/2] library install...
-npm install discord.js ws
-echo.
-echo [2/2] installed
-echo config.json の設定が終わっていることを確認してください。
-echo.
-pause
--- mcws/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Minecraft Discord Bot
-echo Bot Start...
-:loop
-node index.js
-echo.
-echo Botが停止しました。10秒後に再起動します（中断は Ctrl+C）。
-timeout /t 10
-goto loop
